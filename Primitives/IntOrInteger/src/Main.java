e0a57671c66a47b1ff7035383dd1226f1523b3ca
Marina Robionek <marinarobionek@gmail.com>
2023-09-25 11:07:17 +0300

    homework 7 done

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        for(int i = 0; i <= 65536; i++) {
+            char c = (char)i;
+            if(c == 'Ё'|| c == 'ё') {
+                System.out.println(c + " - " + i);
+            } else if(c >= 'А' && c <= 'я') {
+                System.out.println(c + " - " + i);
+            }
+        }
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
